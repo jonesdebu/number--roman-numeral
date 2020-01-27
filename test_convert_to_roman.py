995161d761704ef9f5d64df86ed4a0c6c55f6347
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-01-27 17:28:11 -0500

    added basic tests

--- test_convert_to_roman.py
+++ test_convert_to_roman.py
@@ -4,5 +4,16 @@ import pytest
 from convert_to_roman import to_roman
 
 def test_to_roman():
-    assert to_roman(1) == "I"
     assert to_roman(1000) == "M"
+    assert to_roman(900) == "CM"
+    assert to_roman(500) == "D"
+    assert to_roman(400) == "CD"
+    assert to_roman(100) == "C"
+    assert to_roman(90) == "XC"
+    assert to_roman(50) == "L"
+    assert to_roman(40) == "XL"
+    assert to_roman(10) == "X"
+    assert to_roman(9) == "IX"
+    assert to_roman(5) == "V"
+    assert to_roman(4) == "IV"
+    assert to_roman(1) == "I"
